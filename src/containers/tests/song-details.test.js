d2a3cf7a089836c5051b995b8ef304ab6c868ce7
Martin MacDonald <martin.macdonald01@bbc.co.uk>
2020-03-08 16:09:52 +0000

    Removed uneccesary mock

--- src/containers/tests/song-details.test.js
+++ src/containers/tests/song-details.test.js
@@ -3,10 +3,6 @@ import { render } from "@testing-library/react";
 import { SongDetails } from "../SongDetails";
 import mockSongData from "../../helpers/mock-data";
 
-jest.mock("../../redux/selectors", () => ({
-  getRockSongById: () => mockSongData.results[0]
-}));
-
 test("`SongDetails` renders with all the correct song details", () => {
   const { getByText } = render(<SongDetails song={mockSongData.results[0]} />);
 
